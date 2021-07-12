2a45f8a50a283a1812f0f1b98eb0115beadce4e4
utibeabasi6 <utibeabasiumanah6@gmail.com>
2021-07-12 08:03:48 +0100

    removed second server

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,29 +6,16 @@ provider "aws" {
 resource "aws_instance" "instance1" {
   ami = "ami-0277b52859bac6f4b"
   instance_type = "t2.micro"
+  iam_instance_profile  = "Chefio-server-role"
   key_name = "mt5"
   tags = {
-      Name = "Chefio server 1",
+      Name = "Chefio server ",
       App = "Chefio"
   }
 }
 
-resource "aws_instance" "instance2" {
-  ami = "ami-0277b52859bac6f4b"
-  instance_type = "t2.micro"
-  key_name = "mt5"
-  tags = {
-      Name = "Chefio server 2",
-      App = "Chefio"
-  }
-}
 
 resource "aws_eip" "instance1_eip"{
   instance = aws_instance.instance1.id
-  vpc      = true
-}
-
-resource "aws_eip" "instance2_eip"{
-  instance = aws_instance.instance2.id
   vpc      = true
 }
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,6 +2,3 @@ output "Instance1_eip"{
     value = aws_eip.instance1_eip.public_ip
 }
 
-output "Instance2_eip"{
-    value = aws_eip.instance2_eip.public_ip
-}
